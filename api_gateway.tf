7e74283f1faff358054ee6549a9a2da7c5788fc7
Fabrice Triboix <ftriboix@incise.co>
2022-08-12 20:20:10 +0100

    Remove commented out code

--- api_gateway.tf
+++ api_gateway.tf
@@ -36,10 +36,3 @@ resource "aws_lambda_permission" "api_invoke_lambda" {
   // https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:eu-west-1:${data.aws_caller_identity.this.account_id}:${aws_api_gateway_rest_api.this.id}/*/${aws_api_gateway_method.get_widget.http_method}${aws_api_gateway_resource.widget.path}"
 }
-
-// XXX resource "aws_api_gateway_method_response" "get_widget" {
-// XXX   rest_api_id = aws_api_gateway_rest_api.this.id
-// XXX   resource_id = aws_api_gateway_resource.widget.id
-// XXX   http_method = aws_api_gateway_method.get_widget.http_method
-// XXX   status_code = "500"
-// XXX }
