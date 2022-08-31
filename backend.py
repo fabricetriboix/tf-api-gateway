40e1e5e806bfb8b0ba07e9456c5b66e7b503c749
Fabrice Triboix <ftriboix@incise.co>
2022-08-31 17:40:33 +0100

    The backend Lambda function should return the object, not a JSON string

--- backend.py
+++ backend.py
@@ -1,11 +1,8 @@
-import json
-
 def entry_point(event, context):
     print(f"Received event: {event}")
     response = {
         'statusCode': 200,
         'body': "Backend Lambda version 1"
     }
-    tmp = json.dumps(response)
-    print(f"Returning: {tmp}")
-    return tmp
+    print(f"Returning: {response}")
+    return response
