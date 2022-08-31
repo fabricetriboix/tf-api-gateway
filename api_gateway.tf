a5d7507670bc27f88ce847b10eb53d06554d8049
Fabrice Triboix <ftriboix@incise.co>
2022-08-31 18:12:59 +0100

    Create deployment only after method is created and configured

--- api_gateway.tf
+++ api_gateway.tf
@@ -38,6 +38,7 @@ resource "aws_lambda_permission" "api_invoke_lambda" {
 }
 
 resource "aws_api_gateway_deployment" "this" {
+  depends_on = [aws_api_gateway_integration.get_widget]
   rest_api_id = aws_api_gateway_rest_api.this.id
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.this.body))
